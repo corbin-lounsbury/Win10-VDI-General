e3b090cb66979c7bb04b3ee6fcc43fb44791c235
Corbin <corbin.lounsbury@gmail.com>
2022-01-12 11:20:14 -0500

    Add installation code

--- scripts/applications/install-off2019.ps1
+++ scripts/applications/install-off2019.ps1
@@ -1,11 +1,14 @@
 function install-office {
     $url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_14527-20178.exe"
-    $destination = "c:\temp\office"
+    $destinationPath = "c:\temp\office"
+    $odtArchive = "$destinationPath\ODTarchive.exe"
     write-output "Creating temporary directory"
-    new-item -path $destination -ItemType Directory -Force
-    Invoke-WebRequest -Uri $url -OutFile $destination
+    new-item -path $destinationPath -ItemType Directory -Force
+    Invoke-WebRequest -Uri $url -OutFile $odtArchive
+    Write-Output "Expanding ODT Archive"
+    start-process .\ODTarchive.exe -ArgumentList "/extract:$destinationPath /quiet /passive" -Wait -NoNewWindow
     write-output "Office installation starting"
-    #Start-Process
+    Start-Process $destinationPath\setup.exe -ArgumentList "/configure a:\Office19Configuration.xml" -Wait -NoNewWindow
 }
 
 install-office
