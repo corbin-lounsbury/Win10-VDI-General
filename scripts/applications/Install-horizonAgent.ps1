5f9223361fc7a7f9dd63ddaf9f2c7d3da67a47e9
Corbin <corbin.lounsbury@gmail.com>
2022-01-13 09:46:38 -0500

    Add horizon agent installation

---
+++ scripts/applications/Install-horizonAgent.ps1
@@ -0,0 +1,1 @@
+start-process "\\blap-sto3\isovol1\VMWare\horizon 8\2111\VMware-Horizon-Agent-x86_64-2111.1-8.4.0-19066669.exe" -argumentlist '/s /v "/qn VDM_VC_MANAGED_AGENT=1 ADDLOCAL=Core,SVIAgent,ThinPrint,USB,RTAV"'
