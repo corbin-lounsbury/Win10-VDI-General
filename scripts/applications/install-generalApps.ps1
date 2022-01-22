31eafaf2394670acd5af675d73819f8ddc679d17
Corbin <corbin.lounsbury@gmail.com>
2022-01-22 17:39:17 -0500

    Correct firefox web calls

--- scripts/applications/install-generalApps.ps1
+++ scripts/applications/install-generalApps.ps1
@@ -24,7 +24,7 @@ function get-firefox_ent #Gets most recent version of firefox enterprise from we
         $uriSource = "https://product-details.mozilla.org/1.0/firefox_versions.json"
  
         # Read the JSON and convert to a PowerShell object
-        $firefoxVersions = (Invoke-WebRequest -uri $uriSource).Content | ConvertFrom-Json
+        $firefoxVersions = (Invoke-WebRequest -uri $uriSource -UseBasicParsing).Content | ConvertFrom-Json
  
         $VersionURL = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/$($firefoxVersions.FIREFOX_ESR)/$($architecture)/$($culture)/Firefox%20Setup%20$($firefoxVersions.FIREFOX_ESR).msi"
         #Write-Output $VersionURL
@@ -44,7 +44,7 @@ function get-firefox_ent #Gets most recent version of firefox enterprise from we
         $uriSource = "https://product-details.mozilla.org/1.0/firefox_versions.json"
  
         # Read the JSON and convert to a PowerShell object
-        $firefoxVersions = (Invoke-WebRequest -uri $uriSource).Content | ConvertFrom-Json
+        $firefoxVersions = (Invoke-WebRequest -uri $uriSource -UseBasicParsing ).Content | ConvertFrom-Json
  
         $Version = [Version]$firefoxVersions.FIREFOX_ESR.replace("esr","")
         #Write-Output $Version
@@ -57,7 +57,7 @@ function get-firefox_ent #Gets most recent version of firefox enterprise from we
     $Source = "$PackageName" + "." + "$InstallerType"
     $url = "$(get-LatestFirefoxESRURL)"
     #add-content -path .\firefox\ff-version.info -Value "$Version.0"
-    Invoke-WebRequest -Uri $url -OutFile .\firefox\$Source
+    Invoke-WebRequest -Uri $url -UseBasicParsing -OutFile .\firefox\$Source
     Write-Output "Firefox binary grab version $version complete, starting install"
     start-process msiexec.exe -ArgumentList "/i $working_dir\firefox\Firefox.msi /quiet" -wait -nonewwindow
  
