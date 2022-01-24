b57c2dda17fd43c1d9d5ec432294e83e668b4343
Corbin <corbin.lounsbury@gmail.com>
2022-01-24 11:42:27 -0500

    Change autologon

--- scripts/finalize-image.ps1
+++ scripts/finalize-image.ps1
@@ -1,6 +1,3 @@
 
 #Remove default desktop layout
 Remove-item C:\Users\Public\Desktop\* -Force
-
-# Removes local admin autologon
-Set-ItemProperty -path 'registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Value 0 -Force
